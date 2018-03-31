cdf7f6e19ddeb34020769eaeca6858143d482336
Alexis Romero <romeroalexism@gmail.com>
2018-03-30 21:39:48 -0300

    "cargado de datos a tabla competencia"

--- servidor/dataBase.sql
+++ servidor/dataBase.sql
@@ -3,3 +3,5 @@ CREATE TABLE `competencia` (
   `nombre` varchar(70) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 )
+
+INSERT INTO `competencia` VALUES (1, "Peor pelicula del año"), (2, "Mejor pelicula comica"), (3,"Pelicula màs terrorifica");
