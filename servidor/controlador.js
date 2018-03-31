f6992861211f0d7d2f4fda77771e2cbd4d0ab5c3
Alexis Romero <romeroalexism@gmail.com>
2018-03-30 23:16:01 -0300

    "Conexion conpetencias back con front 1.2"

--- servidor/controlador.js
+++
@@ -1,1 +0,0 @@
-var conexion = require('../conexionDB.js');
--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -1,1 +1,16 @@
-var conexion = require('../conexionDB.js');
+var conexion = require('../baseDeDatos/conexionDB.js');
+
+function buscarTodasLasCompetencias(req, res){
+  var sql = "select * from competencia";
+  conexion.query(sql, function(error, resultado, fields){
+    if(error){
+      console.log("Hubo un error en la consulta", error.message);
+      return res.status(404).send("Hubo un error en la consulta");
+    }
+    res.send(JSON.stringify(resultado));
+  });
+}
+
+module.exports = {
+    buscarTodasLasCompetencias: buscarTodasLasCompetencias
+  }
