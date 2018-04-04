553cfe0678651f51faf55a62afc448fe353c0db5
Alexis Romero <romeroalexism@gmail.com>
2018-04-04 11:17:07 -0300

    "corecciones script sql"

--- servidor/baseDeDatos/dump_base_de_datos.sql
+++ servidor/baseDeDatos/dump_base_de_datos.sql
@@ -109,7 +109,7 @@ CREATE TABLE `competencia` (
   FOREIGN KEY (`actor_id`) REFERENCES `actor` (`id`),
   FOREIGN KEY (`director_id`) REFERENCES `director` (`id`),
   FOREIGN KEY (`genero_id`) REFERENCES `genero` (`id`)
-)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `voto` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -118,4 +118,4 @@ CREATE TABLE `voto` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`competencia_id`) REFERENCES `competencia` (`id`),
   FOREIGN KEY (`pelicula_id`) REFERENCES `pelicula` (`id`)
-)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
