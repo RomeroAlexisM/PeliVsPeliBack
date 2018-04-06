aebcb5952f1643c1897620146f24b2a03c34ab89
Alexis Romero <romeroalexism@gmail.com>
2018-04-06 00:45:01 -0300

    "votacion terminada falta reseteo"

--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -1,13 +1,13 @@
 var conexion = require('../baseDeDatos/conexionDB.js');
 
 function buscarTodasLasCompetencias(req, res){
-  var sql = "select * from competencia where estado = 1";
+  var sql = "SELECT * FROM competencia WHERE estado = 1";
   buscarDatosEnBD(sql, res);
 }
 
 function buscarCompetencia(req, res){
   var idCompetencia = req.params.id;
-  var sql = "select * from competencia where id="+idCompetencia;
+  var sql = "SELECT * FROM competencia WHERE id="+idCompetencia;
   conexion.query(sql, function(error, resultado, fields){
     if(error){
       console.log("Hubo un error en la consulta", error.message);
@@ -36,53 +36,53 @@ function crearSqlBuscarDatosDeCompetencia(idCompetencia, actor_id, director_id, 
   if (existeElActor(actor_id)) {
     if (existeElDirector(director_id)) {
       if (existeElGenero(genero_id)) {
-        return "select competencia.nombre, actor.nombre as actor_nombre, director.nombre as director_nombre, genero.nombre as genero_nombre"+
-              " from competencia, actor, director, genero"+
-              " where competencia.actor_id = actor.id and competencia.director_id = director.id and"+
-              " competencia.genero_id = genero.id and competencia.id = "+idCompetencia;
+        return "SELECT competencia.nombre, actor.nombre AS actor_nombre, director.nombre AS director_nombre, genero.nombre AS genero_nombre"+
+              " FROM competencia, actor, director, genero"+
+              " WHERE competencia.actor_id = actor.id AND competencia.director_id = director.id AND"+
+              " competencia.genero_id = genero.id AND competencia.id = "+idCompetencia;
       }else {
-        return "select competencia.nombre, actor.nombre as actor_nombre, director.nombre as director_nombre"+
-              " from competencia, actor, director"+
-              " where competencia.actor_id = actor.id and competencia.director_id = director.id and"+
+        return "SELECT competencia.nombre, actor.nombre AS actor_nombre, director.nombre AS director_nombre"+
+              " FROM competencia, actor, director"+
+              " WHERE competencia.actor_id = actor.id AND competencia.director_id = director.id AND"+
               " competencia.id = "+idCompetencia;
       }
     }else if (existeElGenero(genero_id)){
-      return "select competencia.nombre, actor.nombre as actor_nombre, genero.nombre as genero_nombre"+
-            " from competencia, actor, genero"+
-            " where competencia.actor_id = actor.id and "+
-            " competencia.genero_id = genero.id and competencia.id = "+idCompetencia;
+      return "SELECT competencia.nombre, actor.nombre AS actor_nombre, genero.nombre AS genero_nombre"+
+            " FROM competencia, actor, genero"+
+            " WHERE competencia.actor_id = actor.id AND "+
+            " competencia.genero_id = genero.id AND competencia.id = "+idCompetencia;
     }else {
-      return "select competencia.nombre, actor.nombre as actor_nombre"+
-            " from competencia, actor"+
-            " where competencia.actor_id = actor.id and"+
+      return "SELECT competencia.nombre, actor.nombre AS actor_nombre"+
+            " FROM competencia, actor"+
+            " WHERE competencia.actor_id = actor.id AND"+
             " competencia.id = "+idCompetencia;
     }
   }else if (existeElDirector(director_id)) {
     if (existeElGenero(genero_id)) {
-      return "select competencia.nombre, director.nombre as director_nombre, genero.nombre as genero_nombre"+
-            " from competencia, director, genero"+
-            " where competencia.director_id = director.id and"+
-            " competencia.genero_id = genero.id and competencia.id = "+idCompetencia;
+      return "SELECT competencia.nombre, director.nombre AS director_nombre, genero.nombre AS genero_nombre"+
+            " FROM competencia, director, genero"+
+            " WHERE competencia.director_id = director.id AND"+
+            " competencia.genero_id = genero.id AND competencia.id = "+idCompetencia;
     }else {
-      return "select competencia.nombre, director.nombre as director_nombre"+
-            " from competencia, director"+
-            " where competencia.director_id = director.id and"+
+      return "SELECT competencia.nombre, director.nombre AS director_nombre"+
+            " FROM competencia, director"+
+            " WHERE competencia.director_id = director.id AND"+
             " competencia.id = "+idCompetencia;
     }
 }else if (existeElGenero(genero_id)) {
-    return "select competencia.nombre, genero.nombre as genero_nombre"+
-          " from competencia, genero"+
-          " where competencia.genero_id = genero.id and competencia.id = "+idCompetencia;
+    return "SELECT competencia.nombre, genero.nombre AS genero_nombre"+
+          " FROM competencia, genero"+
+          " WHERE competencia.genero_id = genero.id AND competencia.id = "+idCompetencia;
   }else {
-    return "select competencia.nombre"+
-          " from competencia"+
-          " where competencia.id = "+idCompetencia;
+    return "SELECT competencia.nombre"+
+          " FROM competencia"+
+          " WHERE competencia.id = "+idCompetencia;
   }
 }
 
 function cargarCompetencia(req, res){
   var idCompetencia = req.params.id;
-  var sql = "select * from competencia where id="+idCompetencia;
+  var sql = "SELECT * FROM competencia WHERE id="+idCompetencia;
   conexion.query(sql, function(error, resultado, fields){
     if(error){
       console.log("Hubo un error en la consulta", error.message);
@@ -126,50 +126,50 @@ function guardarPeliculaOfrecida(idCompetencia, idPelicula){
 
 function crearSqlObtenerPeliculas(actor, director, genero){
   var sql = " SELECT pelicula.id, pelicula.poster, pelicula.titulo ";
-  var seleccionAleatoria = " ORDER BY rand() LIMIT 2"
+  var seleccionAleatoria = " ORDER BY rAND() LIMIT 2"
   if (existeElActor(actor)) {
     if (existeElDirector(director)) {
       if (existeElGenero(genero)) {
         return sql + ", actor.nombre, director.nombre, genero.nombre"+
               " FROM pelicula, actor, director, genero, actor_pelicula, director_pelicula"+
-              " WHERE actor_pelicula.actor_id =actor.id and actor_pelicula.pelicula_id = pelicula.id and"+
-              " pelicula.genero_id = genero.id  and director_pelicula.director_id = director.id and director_pelicula.pelicula_id= pelicula.id and"+
-              " actor.id ="+actor+" and director.id = "+director+" and genero.id = "+genero + seleccionAleatoria;
+              " WHERE actor_pelicula.actor_id =actor.id AND actor_pelicula.pelicula_id = pelicula.id AND"+
+              " pelicula.genero_id = genero.id  AND director_pelicula.director_id = director.id AND director_pelicula.pelicula_id= pelicula.id AND"+
+              " actor.id ="+actor+" AND director.id = "+director+" AND genero.id = "+genero + seleccionAleatoria;
       }else {
         return sql + ", actor.nombre, director.nombre"+
               " FROM pelicula, actor, director, actor_pelicula, director_pelicula"+
-              " WHERE actor_pelicula.actor_id =actor.id and actor_pelicula.pelicula_id = pelicula.id and"+
-              " director_pelicula.director_id = director.id and director_pelicula.pelicula_id= pelicula.id and"+
-              " actor.id ="+actor+" and director.id = "+director + seleccionAleatoria;
+              " WHERE actor_pelicula.actor_id =actor.id AND actor_pelicula.pelicula_id = pelicula.id AND"+
+              " director_pelicula.director_id = director.id AND director_pelicula.pelicula_id= pelicula.id AND"+
+              " actor.id ="+actor+" AND director.id = "+director + seleccionAleatoria;
       }
     }else if (existeElGenero(genero)) {
       return sql + ", actor.nombre, genero.nombre"+
             " FROM pelicula, actor, genero, actor_pelicula"+
-            " WHERE actor_pelicula.actor_id =actor.id and actor_pelicula.pelicula_id = pelicula.id and"+
+            " WHERE actor_pelicula.actor_id =actor.id AND actor_pelicula.pelicula_id = pelicula.id AND"+
             " pelicula.genero_id = genero.id"+
-            " actor.id ="+actor+" and genero.id = "+genero + seleccionAleatoria;
+            " actor.id ="+actor+" AND genero.id = "+genero + seleccionAleatoria;
     }else {
       return sql + ", actor.nombre"+
             " FROM pelicula, actor, actor_pelicula"+
-            " WHERE actor_pelicula.actor_id =actor.id and actor_pelicula.pelicula_id = pelicula.id and"+
+            " WHERE actor_pelicula.actor_id =actor.id AND actor_pelicula.pelicula_id = pelicula.id AND"+
             " actor.id ="+actor + seleccionAleatoria;
     }
   }else if (existeElDirector(director)) {
     if (existeElGenero(genero)) {
       return sql + ", director.nombre, genero.nombre"+
             " FROM pelicula, director, genero, director_pelicula"+
-            " WHERE pelicula.genero_id = genero.id  and director_pelicula.director_id = director.id and director_pelicula.pelicula_id= pelicula.id and"+
-            " director.id = "+director+" and genero.id = "+genero + seleccionAleatoria;
+            " WHERE pelicula.genero_id = genero.id  AND director_pelicula.director_id = director.id AND director_pelicula.pelicula_id= pelicula.id AND"+
+            " director.id = "+director+" AND genero.id = "+genero + seleccionAleatoria;
     }else {
       return sql + ", director.nombre"+
             " FROM pelicula, director, director_pelicula"+
-            " WHERE director_pelicula.director_id = director.id and director_pelicula.pelicula_id= pelicula.id and "+
+            " WHERE director_pelicula.director_id = director.id AND director_pelicula.pelicula_id= pelicula.id AND "+
             " director.id = "+director + seleccionAleatoria;
     }
   }else if (existeElGenero(genero)) {
     return sql + ", genero.nombre"+
           " FROM pelicula, genero"+
-          " WHERE pelicula.genero_id = genero.id and"+
+          " WHERE pelicula.genero_id = genero.id AND"+
           " genero.id = "+genero + seleccionAleatoria;
   }else {
     return sql + "FROM pelicula" + seleccionAleatoria;
@@ -242,33 +242,11 @@ function sumarVotoDePelicula(req, res){
   manipularDatosEnBD(sql, res);
 }
 
-// function devolverResultadoVotacion(req, res){
-//   var idCompetencia = req.params.id;
-//   var sql = "SELECT competencia.nombre, pelicula.id, pelicula.titulo, pelicula.poster,"+
-//             " COUNT(voto.pelicula_id) AS votos FROM voto, pelicula,"+
-//             " competencia WHERE pelicula.id = voto.pelicula_id"+
-//             " AND competencia.id = voto.competencia_id AND competencia.id = "+idCompetencia+
-//             " GROUP BY voto.pelicula_id ORDER BY votos desc LIMIT 3";
-//   conexion.query(sql, function(error, resultado, fields){
-//     if(error){
-//       console.log("Hubo un error en la consulta", error.message);
-//       return res.status(404).send("Hubo un error en la consulta");
-//     }
-//     if (resultado.length != 0) {
-//       var response = {
-//         'resultados': resultado,
-//         'competencia': resultado[0].nombre,
-//       }
-//       res.send(JSON.stringify(response));
-//     }
-//   });
-// }
-
 function devolverResultadoVotacion(req, res){
   var idCompetencia = req.params.id;
-  var sql = "select count(voto.pelicula_id) as votos, pelicula.id as pelicula_id"+
-            " from voto,pelicula where voto.pelicula_id = pelicula.id"+
-            " AND voto.competencia_id = "+idCompetencia+" group by voto.pelicula_id";
+  var sql = "SELECT COUNT(voto.pelicula_id) AS votos, pelicula.id AS pelicula_id"+
+            " FROM voto,pelicula WHERE voto.pelicula_id = pelicula.id"+
+            " AND voto.competencia_id = "+idCompetencia+" GROUP BY voto.pelicula_id";
   conexion.query(sql, function(error, resultado, fields){
     if(error){
       console.log("Hubo un error en la consulta", error.message);
@@ -279,63 +257,63 @@ function devolverResultadoVotacion(req, res){
 }
 
 function calcularVotos(idCompetencia, datosPeliculas, res){
-  var sql = "select competencia.nombre, pelicula.titulo, pelicula.poster,"+
-            " count(pelicula_ofrecida.pelicula_id) as apariciones, pelicula.id as pelicula_id"+
-            " from pelicula_ofrecida, pelicula, voto, competencia where pelicula_ofrecida.pelicula_id = pelicula.id"+
+  var sql = "SELECT competencia.nombre, pelicula.titulo, pelicula.poster,"+
+            " COUNT(pelicula_ofrecida.pelicula_id) AS apariciones, pelicula.id AS pelicula_id"+
+            " FROM pelicula_ofrecida, pelicula, voto, competencia WHERE pelicula_ofrecida.pelicula_id = pelicula.id"+
             " AND pelicula_ofrecida.competencia_id = "+idCompetencia+" AND voto.pelicula_id = pelicula_ofrecida.pelicula_id "+
-            " AND competencia.id = pelicula_ofrecida.competencia_id group by pelicula_ofrecida.pelicula_id"; //+
-            // " order by apariciones desc";
+            " AND competencia.id = pelicula_ofrecida.competencia_id GROUP BY pelicula_ofrecida.pelicula_id";
   conexion.query(sql, function(error, resultado, fields){
-    var votos = [];
-    var aux = 0;
+    var resultados = [];
+    var totalDePeliculas = 0;
     if(error){
       console.log("Hubo un error en la consulta", error.message);
       return res.status(404).send("Hubo un error en la consulta");
     }
     for (var i = 0; i < resultado.length; i++) {
         if (resultado[i].pelicula_id == datosPeliculas[i].pelicula_id) {
-
-        votos[i] ={
-          'votos':  Math.round(resultado[i].apariciones/datosPeliculas[i].votos),
+        resultados[i] ={
+          'votos':  Math.round((datosPeliculas[i].votos/resultado[i].apariciones)*10),//Se divide la cantidad de votos con respecto a las apariciones de la pelicula
           'pelicula_id': resultado[i].pelicula_id,
           'poster': resultado[i].poster,
           'titulo': resultado[i].titulo
         }
-        aux ++;
+        totalDePeliculas ++;
         }
     }
-    if (resultado.length == aux) {
-      console.log(votos);
-      // var response ={
-      //   'competencia': resultado[0].nombre,
-      //   'resultados': votos,
-      // }
-      // res.send(JSON.stringify(response));
+    if (resultado.length == totalDePeliculas) {
+      //ordena el objeto resutlados de mayor a menos
+      resultados.sort(function (a, b){//fuente: https://comoprogramo.wordpress.com/2012/06/29/como-ordenar-en-javascript-un-array-de-objetos-por-cualquier-campo/
+        return (b.votos - a.votos)
+      })
+      var response ={
+        'competencia': resultado[0].nombre,
+        'resultados': resultados,
+      }
+      res.send(JSON.stringify(response));
     }
   });
 }
-
-
-
 
 function reiniciarVotacion(req, res){
   var idCompetencia = req.params.idCompetencia;
   var sql = "DELETE voto FROM voto INNER JOIN competencia ON voto.competencia_id = competencia.id WHERE competencia.id = "+idCompetencia ;
   manipularDatosEnBD(sql, res);
+  // var sql = "DELETE pelicula_ofrecida FROM pelicula_ofrecida INNER JOIN competencia ON pelicula_ofrecida.competencia_id = competencia.id WHERE competencia.id = "+idCompetencia ;
+  // manipularDatosEnBD(sql, res);
 }
 
 function cargarGeneros(req, res){
-  var sql = "Select * from genero";
+  var sql = "SELECT * FROM genero";
   buscarDatosEnBD(sql, res);
 }
 
 function cargarActores(req, res){
-  var sql = "Select * from actor";
+  var sql = "SELECT * FROM actor";
   buscarDatosEnBD(sql, res);
 }
 
 function cargarDirectores(req, res){
-  var sql = "Select * from director";
+  var sql = "SELECT * FROM director";
   buscarDatosEnBD(sql, res);
 }
 
